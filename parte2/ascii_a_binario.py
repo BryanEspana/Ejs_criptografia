e6126a1062a69d30ca187610b8e0b01ccbf1678f
Bryan España <83832357+BryanEspana@users.noreply.github.com>
2026-01-22 18:53:15 -0600

    ascii a binario

---
+++ parte2/ascii_a_binario.py
@@ -0,0 +1,18 @@
+texto = "hoasdfdc"
+
+def texto_a_ascii(texto):
+    texto_en_ascii = ""
+    for caracter in texto:
+        #ASCII
+        ascii_decimal = ord(caracter) 
+        texto_en_ascii += str(ascii_decimal).strip() + " "
+    return texto_en_ascii
+
+def ascii_a_binario(texto_en_ascii)
+    for caracter in texto_en_ascii
+        resultado_division = caracter / 2
+    
+
+
+
+print(texto_a_ascii(texto))
